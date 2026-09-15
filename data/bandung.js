c9c93b1a6228d84e1d142b77e780aa5071a41906
LIONPARCELBANDUNG-POS101 <lp.bds.citarik.cicalengka@gmail.com>
2026-09-15 14:56:44 +0700

    Update bandung.js

--- data/bandung.js
+++ data/bandung.js
@@ -221,7 +221,8 @@ var bandungData = [
         kategori: "Pengen Jalan-jalan / Healing",
         jenis: "Wisata Budaya",
         deskripsi: "Wisata seni dan budaya Sunda dengan pertunjukan angklung, tari, wayang, dan pengalaman interaktif.",
-        link: "https://angklung-udjo.co.id/"
+        link: "https://angklung-udjo.co.id/",
+        foto: "images/bandung/saung ujo.jpg"
     },
 
     {
@@ -230,6 +231,7 @@ var bandungData = [
         jenis: "Museum & Edukasi",
         deskripsi: "Museum yang cocok untuk wisata edukasi dan mengenal sejarah geologi Indonesia.",
         link: "#"
+         foto: "images/bandung/museumgeologi.jpg"
     },
 
     {
@@ -238,6 +240,7 @@ var bandungData = [
         jenis: "Wisata Religi",
         deskripsi: "Destinasi wisata religi dan arsitektur yang menjadi salah satu ikon Jawa Barat.",
         link: "#"
+         foto: "images/bandung/masjid aljabar.jpg"
     },
 
     {
